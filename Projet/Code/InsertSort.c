aee29934076a8fad0b69feb62b066ff493d3105d
ilyes2000 <36744724+ilyes2000@users.noreply.github.com>
2021-04-01 19:37:53 +0100

    Add files via upload

--- Projet/Code/InsertSort.c
+++ Projet/Code/InsertSort.c
@@ -3,26 +3,28 @@
 #include<time.h>
 
 int main(){
-    long int tab[1000000];
-    long int i, j, tmp;
-    long int n[]={50000, 100000, 200000, 400000, 800000};
-    clock_t t1,t2;
+    long int tab[100000];
+    long int i, j, tmp, n;
     
-    int q=0;
-    while (q<7){
-    for (i = 0; i < n[q]; i++) tab[i]=rand();
-    t1=clock();
-    for (i = 0; i < n[q]; i++){
-        j=i-1; tmp=tab[i];
+    printf("combien d'elements contient le tableau ?");
+    scanf("%d",&n);
+    printf("\nremplissez le tableau: ");
+    for (i = 0; i < n; i++) scanf("%d",&tab[i]);
+
+    for (i = 0; i < n; i++){
+        j=i-1; 
+        tmp=tab[i];
         while(j>=0 && tab[j]>tmp) {
             tab[j+1]=tab[j];
             j=j-1;
         }
         tab[j+1]=tmp;
     }
-    t2=clock();
-    printf("\nle temps d'exec est: %f",(double)(t2-t1)/CLOCKS_PER_SEC);
-    q++;
-    }
+
+    printf("le tableau triee:");
+    printf("[");
+    for (i = 0; i < n; i++) printf("\t%d,",tab[i]);
+    printf("]");
+
     return 0;    
 }
